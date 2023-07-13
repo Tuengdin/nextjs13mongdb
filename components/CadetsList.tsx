f91b81ae3790edf0e4cd52f6a6d32d3aa0de5cf4
tdin <tuengdin@gmail.com>
2023-07-13 22:02:31 +0700

    add title of data in CadetsList.tsx

--- components/CadetsList.tsx
+++ components/CadetsList.tsx
@@ -40,15 +40,15 @@ export default async function CadetsList() {
           className="bg-slate-200/75 p-4  my-3 flex justify-between gap-5 items-start"
         >
           <div>
-            <h2 className="font-bold text-2xl">{c.login}</h2>
-            <div>{c.firstName}</div>
-            <div>{c.lastName}</div>
-            <div>{c.email}</div>
-            <div>{c.level}</div>
-            <div>{c.bhIn}</div>
-            <div>{c.poolMonth}</div>
-            <div>{c.poolYear}</div>
-            <div>{c.blackholedAt}</div>
+            <h2 className="font-bold text-2xl">Login: {c.login}</h2>
+            <div>Firstname: {c.firstName}</div>
+            <div>Lastname: {c.lastName}</div>
+            <div>Email: {c.email}</div>
+            <div>Level: {c.level}</div>
+            <div>Blackhole in: {c.bhIn}</div>
+            <div>Pool Month: {c.poolMonth}</div>
+            <div>Pool Year: {c.poolYear}</div>
+            <div>Blackholed at: {c.blackholedAt}</div>
           </div>
           <div className="flex gap-2">
             <RemoveBtn />

114d2f6913494ecdaad58d66201819f6e0d37a25
tdin <tuengdin@gmail.com>
2023-07-15 23:13:50 +0700

    Comment EditCadetForm.tsx

--- components/EditCadetForm.tsx
+++ components/EditCadetForm.tsx
@@ -1,32 +1,32 @@
-"use client";
+// "use client";
 
-import Cadet from "@/models/cadet";
-import { useState } from "react";
+// import Cadet from "@/models/cadet";
+// import { useState } from "react";
 
-export default function EditCadetForm({
-  login,
-  level,
-  firstName,
-  lastName,
-  bhIn,
-  poolMonth,
-  poolYear,
-  email,
-  blackholedAt,
-}) {
-  const [newLogin, setNewLogin] = useState(login);
-  const [newFirstName, setNewFirstName] = useState(firstName);
-  const [newLastName, setNewLastName] = useState(lastName);
-  const [newEmail, setNewEmail] = useState(email);
-  const [newLevel, setNewLevel] = useState(level);
-  const [newBhIn, setNewBhIn] = useState(bhIn);
-  const [newPoolMonth, setNewPoolMonth] = useState(poolMonth);
-  const [newPoolYear, setNewPoolYear] = useState(poolYear);
-  const [newBlackholedAt, setNewBlackholedAt] = useState(blackholedAt);
+// export default function EditCadetForm({
+//   login,
+//   level,
+//   firstName,
+//   lastName,
+//   bhIn,
+//   poolMonth,
+//   poolYear,
+//   email,
+//   blackholedAt,
+// }) {
+//   const [newLogin, setNewLogin] = useState(login);
+//   const [newFirstName, setNewFirstName] = useState(firstName);
+//   const [newLastName, setNewLastName] = useState(lastName);
+//   const [newEmail, setNewEmail] = useState(email);
+//   const [newLevel, setNewLevel] = useState(level);
+//   const [newBhIn, setNewBhIn] = useState(bhIn);
+//   const [newPoolMonth, setNewPoolMonth] = useState(poolMonth);
+//   const [newPoolYear, setNewPoolYear] = useState(poolYear);
+//   const [newBlackholedAt, setNewBlackholedAt] = useState(blackholedAt);
 
-  return (
-    <div className="bg-slate-200/75 p-4  my-3 flex flex-col justify-between gap-5 items-start">
-      Edit Topic
-    </div>
-  );
-}
+//   return (
+//     <div className="bg-slate-200/75 p-4  my-3 flex flex-col justify-between gap-5 items-start">
+//       Edit Topic
+//     </div>
+//   );
+// }
